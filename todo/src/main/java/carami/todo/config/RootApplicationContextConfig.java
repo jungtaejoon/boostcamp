2dcc9f674af4a3095b55a5a8a2a43b9ac244225e
jungtaejoon <jungtaejoon@naver.com>
2017-07-06 12:15:27 +0900

    dbconnection test 완료

--- todo/src/main/java/carami/todo/config/RootApplicationContextConfig.java
+++ todo/src/main/java/carami/todo/config/RootApplicationContextConfig.java
@@ -1,7 +1,13 @@
 package carami.todo.config;
 
-import org.springframework.context.annotation.Configuration;
+import org.springframework.context.annotation.*;
 
 @Configuration
+// dao, service에 대한 컴포넌트를 scan한다. sevice는 미리 추가한다.
+@ComponentScan(basePackages = {
+        "carami.todo.dao",
+        "carami.todo.service"
+})
+@Import({DbConfig.class}) // DBConfig 를 설정한다.
 public class RootApplicationContextConfig {
 }
